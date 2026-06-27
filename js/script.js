b81e09a23a67d9a4cf17576ab705707cb397522a
yescode23 <rahasia4445@gmail.com>
2026-06-27 19:54:18 +0700

    Enhance navbar with sticky positioning and background color; add hamburger button functionality

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Hamburger Button Function
+const navigationLink = document.querySelector('.navigation');
+const hamburgerToggle = document.getElementById('hamburger-toggle');
+const hamburgerIcon = document.getElementById('hamburger-icon');
+
+hamburgerToggle.addEventListener('click', function() {
+    navigationLink.classList.toggle('active')
+
+    if (navigationLink.classList.contains('active')) {
+        hamburgerIcon.classList.replace('bx-menu', 'bx-x');
+    } else {
+        hamburgerIcon.classList.replace('bx-x', 'bx-menu');
+    }
+})
