2c3d19f3730d3efbe103b80d2e5afc630348ad1b
yescode23 <rahasia4445@gmail.com>
2026-06-29 00:54:52 +0700

    Implement dark mode functionality with toggle and styling adjustments

--- js/script.js
+++ js/script.js
@@ -15,3 +15,31 @@ hamburgerToggle.addEventListener('click', function() {
 
 // Dark Mode Button Function
 const darkModeToggle = document.getElementById('dark-mode-toggle');
+const bodyElement = document.body;
+const currentThemeKey = localStorage.getItem('theme');
+
+function enableDarkMode() {
+    bodyElement.classList.add('dark-mode');
+    darkModeToggle.innerHTML = '<i class="bx bx-sun-bright"></i> Light Mode';
+    localStorage.setItem('theme', 'dark-mode');
+}
+
+function disableDarkMode() {
+    bodyElement.classList.remove('dark-mode');
+    darkModeToggle.innerHTML = '<i class="bx bx-moon"></i> Dark Mode';
+    localStorage.setItem('theme', 'light-mode');
+}
+
+if (currentThemeKey === 'dark-mode') {
+    enableDarkMode();
+} else {
+    disableDarkMode();
+}
+
+darkModeToggle.addEventListener('click', function() {
+    if (bodyElement.classList.contains('dark-mode')) {
+        disableDarkMode();
+    } else {
+        enableDarkMode();
+    }
+})
